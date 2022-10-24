7e113da9f7f2ce7bccb33090de88cf80c6bc11e2
Patrik Nordwall <patrik.nordwall@gmail.com>
2022-10-24 12:17:34 +0200

    Change Kafka producer, eventsBySlices etc

--- samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/PublishKafkaEventsProjection.scala
+++ samples/grpc/shopping-cart-service-scala/src/main/scala/shopping/cart/PublishKafkaEventsProjection.scala
@@ -7,13 +7,16 @@ import akka.cluster.sharding.typed.scaladsl.ShardedDaemonProcess
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.SendProducer
 import akka.persistence.query.Offset
+import akka.persistence.query.typed.EventEnvelope
 import akka.persistence.r2dbc.query.javadsl.R2dbcReadJournal
-import akka.projection.eventsourced.EventEnvelope
 import akka.projection.eventsourced.scaladsl.EventSourcedProvider
 import akka.projection.r2dbc.scaladsl.R2dbcProjection
-import akka.projection.scaladsl.{AtLeastOnceProjection, SourceProvider}
-import akka.projection.{ProjectionBehavior, ProjectionId}
-import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
+import akka.projection.scaladsl.{ AtLeastOnceProjection, SourceProvider }
+import akka.projection.{ ProjectionBehavior, ProjectionId }
+import org.apache.kafka.common.serialization.{
+  ByteArraySerializer,
+  StringSerializer
+}
 
 object PublishKafkaEventsProjection {
 
@@ -22,12 +25,18 @@ object PublishKafkaEventsProjection {
     val topic =
       system.settings.config.getString("shopping-cart-service.kafka.topic")
 
+    val numberOfSliceRanges: Int = 4
+    val sliceRanges = EventSourcedProvider.sliceRanges(
+      system,
+      R2dbcReadJournal.Identifier,
+      numberOfSliceRanges)
+
     ShardedDaemonProcess(system).init(
       name = "PublishEventsProjection",
-      ShoppingCart.tags.size,
+      numberOfInstances = sliceRanges.size,
       index =>
         ProjectionBehavior(
-          createProjectionFor(system, topic, sendProducer, index)),
+          createProjectionFor(system, topic, sendProducer, sliceRanges(index))),
       ShardedDaemonProcessSettings(system),
       Some(ProjectionBehavior.Stop))
   }
@@ -50,22 +59,29 @@ object PublishKafkaEventsProjection {
       system: ActorSystem[_],
       topic: String,
       sendProducer: SendProducer[String, Array[Byte]],
-      index: Int)
+      sliceRange: Range)
       : AtLeastOnceProjection[Offset, EventEnvelope[ShoppingCart.Event]] = {
-    val tag = ShoppingCart.tags(index)
+    val minSlice = sliceRange.min
+    val maxSlice = sliceRange.max
+    val projectionId =
+      ProjectionId("PublishEventsProjection", s"carts-$minSlice-$maxSlice")
+
     val sourceProvider
         : SourceProvider[Offset, EventEnvelope[ShoppingCart.Event]] =
-      EventSourcedProvider.eventsByTag[ShoppingCart.Event](
+      EventSourcedProvider.eventsBySlices[ShoppingCart.Event](
         system = system,
         readJournalPluginId = R2dbcReadJournal.Identifier,
-        tag = tag)
+        ShoppingCart.EntityKey.name,
+        minSlice,
+        maxSlice)
 
     R2dbcProjection.atLeastOnceAsync(
-      projectionId = ProjectionId("PublishEventsProjection", tag),
+      projectionId,
       None,
       sourceProvider,
-      handler =
-        () => new PublishKafkaEventsProjectionHandler(system, topic, sendProducer))(system)
+      handler = () =>
+        new PublishKafkaEventsProjectionHandler(system, topic, sendProducer))(
+      system)
   }
 
 }
